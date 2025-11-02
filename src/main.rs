ccc6d9e0f97aa6a433c493d85ebc198d4ce7224f
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-11-03 00:43:21 +0300

    Add second task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut sum = 0;
+    let mut input = String::new();
+    let mut ok = true;
+    let mut number;
+    let _ = io::stdin().read_line(&mut input);
+    number = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            ok = false;
+            0
+        }
+    };
+    while number != -1 {
+        sum += number;
+        let mut input = String::new();
+        let _ = io::stdin().read_line(&mut input);
+        number = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                ok = false;
+                0
+            }
+        }
+    }
+    if ok {
+        println!("{}", sum);
+    } else {
+        println!("NaN");
+    }
 }
